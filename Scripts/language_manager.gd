89d245dbc49d0a24591e4ff00443d131a27a1274
Ale2301 <alejo.2001@live.com.ar>
2025-05-28 13:49:55 -0300

    fix: created language_manager.gd

---
+++ Scripts/language_manager.gd
@@ -0,0 +1,6 @@
+extends Node
+
+## Este archivo se va a encargar de gestionar la carga de los JSON, y de guardar los idiomas
+
+var textLanguage = "English"
+var soundLanguage = "Spanish"
--- scripts/autoload.gd
+++ scripts/autoload.gd
@@ -18,9 +18,3 @@ func register_enemy(enemy):
 	enemies.append(enemy)
 func _spawn_enemy():
 	enemies[enemyToSpawn].spawn_enemy()
-
-
-## Al poner variables acá, desordeno el flujo del archivo. Pero prefiero esto a mezclar estas variables con las otras
-
-var textLanguage = "English"
-var soundLanguage = "Spanish"
