289971d51a4635d73a705c073bde10910f1df3aa
Lucas Ramirez <lucas.e.ram2006@gmail.com>
2025-07-23 18:33:10 -0300

    Se agregan comentarios, y se borraron archivos innecesarios

--- Scripts/microphoneScript.gd
+++ Scripts/microphoneScript.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var gain_bar = $AudioStreamMicrophone/ProgressBar
+@onready var gain_bar = get_node("/root/Node3D/Player/Node3D/MicInput/AudioStreamMicrophone/ProgressBar")
 var capture = AudioEffectCapture  
 @onready var microphone = get_node("/root/Node3D/Player/Node3D/MicInput/AudioStreamMicrophone")
 
@@ -17,16 +17,17 @@ func _ready():
 
 
 func _process(delta):
+	# se verifica que el capture exista y que tenga al menos 1024 muestras
 	if capture and capture.can_get_buffer(1024):
-
+	# se obtiene los 1024 muestras del microfono como un array de float
 		var buffer = capture.get_buffer(1024)
 
 		if buffer.size() > 0:
-			var sum = 0.0
-			for sample in buffer:
+			var sum = 0.0 
+			for sample in buffer:  # Sumamos el cuadrado de cada muestra (uso de energía)
 				sum += sample.length_squared()  
 
-			var rms = sqrt(sum / buffer.size())
-			var db = 20.0 * log(max(rms, 1e-8)) / log(10.0)
-			db = clamp(db, -60.0, 0.0)
-			gain_bar.value = db
+			var rms = sqrt(sum / buffer.size()) # Calculamos el RMS (root mean square = raíz del promedio de los cuadrados)
+			var db = 20.0 * log(max(rms, 1e-8)) / log(10.0) # se convierte el rms en decibelios con un logaritmo base 10
+			db = clamp(db, -60.0, 0.0) #Se limitan los Db entre -60 (silencio ) y 0 (nivel maximo)
+			gain_bar.value = db # Se asignan los decibelios a la ProgressBar
