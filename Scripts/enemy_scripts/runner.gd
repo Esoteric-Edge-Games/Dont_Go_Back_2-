41fe9f50cd17c15471611028667bd474576350b9
Lucas Ramirez <lucas.e.ram2006@gmail.com>
2025-08-05 17:24:08 -0300

    Adicion del ambiente, Sky, Shaders, Sounds

--- Scripts/enemy_scripts/runner.gd
+++ Scripts/enemy_scripts/runner.gd
@@ -49,7 +49,7 @@ func spawn_enemy():
 	if not runnerOn:
 		runnerOn = true
 		global_position = player.position - Vector3(randf_range(-10, 10), 0, randf_range(5, 20))
-		visible = true  # Haz visible al Runner cuando se spawnea
+		visible = true # Haz visible al Runner cuando se spawnea
 		RunnerMove.play("Moving")  
 		print("Runner spawneo")
 
--- Scripts/sounds.gd
+++ Scripts/sounds.gd
@@ -5,5 +5,12 @@ extends Node3D
 
 func _on_owl_timer_timeout():
 	$Owl.play()
-	$Owl/OwlTimer.start()
-	
+
+func _on_howl_timer_timeout():
+	$Howl.play()
+
+func _on_irons_timer_timeout():
+	$Irons.play()
+
+func _on_branch_breaking_timer_timeout():
+	$"Branch Breaking".play()
